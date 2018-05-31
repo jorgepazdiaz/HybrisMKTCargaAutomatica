b831649e0cacc386097ce98d6b3b54a71ada0426
Carlos Gulfo <carlostevengulfo@gmail.com>
2018-05-31 11:30:46 -0500

    Corrección de la generación de la fecha(Timestamp)

--- dal/dal.py
+++ dal/dal.py
@@ -50,7 +50,7 @@ class SqlServerAccess:
 
 # Obtiene el timestamp actual y trunca la cantidad de caracteres, reemplazando el . que está de más
 def get_timestamp_str():
-    return '/Date('+str(int(time.time()))+')/'
+    return "/Date("+str(int(round(time.time() * 1000)))+")/"
 
 
 def get_timestamp_from_date_str(date_to_parse):
